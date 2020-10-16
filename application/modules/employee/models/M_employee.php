e7b35a34dcc3bc7f5ceb09bd8ad91eb53f916185
chrisnabayuaji <chrisnabayuaji@gmail.com>
2020-10-16 22:29:06 +0700

    Initial commit

---
+++ application/modules/employee/models/M_employee.php
@@ -0,0 +1,85 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_departement extends CI_Model
+{
+
+  public function where($cookie)
+  {
+    $where = "WHERE a.is_deleted = 0 ";
+    if (@$cookie['search']['term'] != '') {
+      $where .= "AND a.departement_name LIKE '%" . $this->db->escape_like_str($cookie['search']['term']) . "%' ";
+    }
+    return $where;
+  }
+
+  public function list_data($cookie)
+  {
+    $where = $this->where($cookie);
+    $sql = "SELECT * FROM departement a 
+      $where
+      ORDER BY "
+      . $cookie['order']['field'] . " " . $cookie['order']['type'] .
+      " LIMIT " . $cookie['cur_page'] . "," . $cookie['per_page'];
+    $query = $this->db->query($sql);
+    return $query->result_array();
+  }
+
+  public function all_data()
+  {
+    $where = "WHERE a.is_deleted = 0 ";
+
+    $sql = "SELECT * FROM departement a $where ORDER BY created_at";
+    $query = $this->db->query($sql);
+    return $query->result_array();
+  }
+
+  public function all_rows($cookie)
+  {
+    $where = $this->where($cookie);
+
+    $sql = "SELECT COUNT(1) as total FROM departement a $where";
+    $query = $this->db->query($sql);
+    return $query->row_array()['total'];
+  }
+
+  function by_field($field, $val)
+  {
+    $sql = "SELECT * FROM departement WHERE $field = ?";
+    $query = $this->db->query($sql, array($val));
+    $row = $query->row_array();
+    return $row;
+  }
+
+  public function save($data, $id = null)
+  {
+    if ($id == null) {
+      $data['created_at'] = date('Y-m-d H:i:s');
+      $data['created_by'] = $this->session->userdata('user_fullname');
+      $this->db->insert('departement', $data);
+    } else {
+      $data['updated_at'] = date('Y-m-d H:i:s');
+      $data['updated_by'] = $this->session->userdata('user_fullname');
+      $this->db->where('departement_id', $id)->update('departement', $data);
+    }
+  }
+
+  public function update($id, $data)
+  {
+    $data['updated_at'] = date('Y-m-d H:i:s');
+    $data['updated_by'] = $this->session->userdata('fullname');
+    $this->db->where('departement_id', $id)->update('departement', $data);
+  }
+
+  public function delete($id, $permanent = true)
+  {
+    if ($permanent) {
+      $this->db->where('departement_id', $id)->delete('departement');
+    } else {
+      $data['is_deleted'] = 1;
+      $data['updated_at'] = date('Y-m-d H:i:s');
+      $data['updated_by'] = $this->session->userdata('user_fullname');
+      $this->db->where('departement_id', $id)->update('departement', $data);
+    }
+  }
+}
---
+++ application/modules/employee/views/form.php
@@ -0,0 +1,126 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-0">
+        <div class="col-sm-6">
+          <h5 class="m-0 text-dark"><i class="<?= @$menu['icon'] ?>"></i> <?= @$menu['menu_name'] ?></h5>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item active">Master Data</li>
+            <li class="breadcrumb-item active"><?= @$menu['menu_name'] ?></li>
+            <li class="breadcrumb-item active"><?= ($id == null) ? 'Tambah' : 'Ubah'; ?></li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div><!-- /.content-header -->
+  <!-- Main content -->
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Form <?= $menu['menu_name'] ?></h3>
+            </div>
+            <form id="form" action="<?= site_url() . '/' . $menu['controller'] . '/save/' . $id ?>" method="post" autocomplete="off" enctype="multipart/form-data">
+              <div class="card-body">
+                <div class="flash-error" data-flasherror="<?= $this->session->flashdata('flash_error') ?>"></div>
+                <?php if ($id != null) : ?>
+                  <input type="hidden" class="form-control form-control-sm" name="old" id="old" value="<?= @$main['employee_id'] ?>" required>
+                <?php endif; ?>
+                <div class="form-group row">
+                  <label for="menu" class="col-sm-2 col-form-label text-right">Kode <span class="text-danger">*</span></label>
+                  <div class="col-sm-2">
+                    <input type="text" class="form-control form-control-sm" name="employee_id" id="employee_id" value="<?= @$main['employee_id'] ?>" required>
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="menu" class="col-sm-2 col-form-label text-right">Nama Departemen <span class="text-danger">*</span></label>
+                  <div class="col-sm-4">
+                    <input type="text" class="form-control form-control-sm" name="employee_name" id="employee_name" value="<?= @$main['employee_name'] ?>" required>
+                  </div>
+                </div>
+                <div id="icon-container" class="form-group row">
+                  <label for="icon" class="col-sm-2 col-form-label text-right">Deskripsi</label>
+                  <div class="col-sm-6">
+                    <input type="text" class="form-control form-control-sm" name="description" id="description" value="<?= @$main['description'] ?>">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="url" class="col-sm-2 col-form-label text-right">Aktif</label>
+                  <div class="col-sm-3">
+                    <div class="pretty p-icon">
+                      <input class="icheckbox" type="checkbox" name="is_active" id="is_active" value="1" <?php if(@$main){echo (@$main['is_active'] == 1) ? 'checked' : '';}else{echo 'checked';}  ?>>
+                      <div class="state">
+                        <i class="icon fas fa-check"></i><label></label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="card-footer">
+                <div class="row">
+                  <div class="col-md-10 offset-md-2">
+                    <button type="submit" class="btn btn-sm btn-primary btn-submit"><i class="fas fa-save"></i> Simpan</button>
+                    <a class="btn btn-sm btn-default btn-cancel" href="<?= site_url() . '/' . $menu['controller'] . '/' . $menu['url'] ?>"><i class="fas fa-times"></i> Batal</a>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </div><!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+<script>
+  $(document).ready(function() {
+    $("#form").validate({
+      rules: {
+        employee_id: {
+          remote: {
+            type: 'post',
+            url: "<?= site_url() . '/' . $menu['controller'] . '/ajax/check_id/' . $id ?>",
+            data: {
+              'employee_id': function() {
+                return $('#employee_id').val();
+              }
+            },
+            dataType: 'json'
+          }
+        }
+      },
+      messages: {
+        employee_id: {
+          remote: "Kode sudah digunakan"
+        }
+      },
+      errorElement: "em",
+      errorPlacement: function(error, element) {
+        error.addClass("invalid-feedback");
+        if (element.prop("type") === "checkbox") {
+          error.insertAfter(element.next("label"));
+        } else {
+          error.insertAfter(element);
+        }
+      },
+      highlight: function(element, errorClass, validClass) {
+        $(element).addClass("is-invalid").removeClass("is-valid");
+      },
+      unhighlight: function(element, errorClass, validClass) {
+        $(element).addClass("is-valid").removeClass("is-invalid");
+      },
+      submitHandler: function(form) {
+        $(".btn-submit").html('<i class="fas fa-spin fa-spinner"></i> Proses');
+        $(".btn-submit").addClass('disabled');
+        $(".btn-cancel").addClass('disabled');
+        form.submit();
+      }
+    });
+  })
+</script>
