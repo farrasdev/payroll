17d0c344bc9bca334164cb32929663c8533856c7
chrisnabayuaji <chrisnabayuaji@gmail.com>
2020-10-22 16:28:22 +0700

    UPdate

--- application/modules/attendance/models/M_attendance.php
+++ application/modules/attendance/models/M_attendance.php
@@ -7,9 +7,9 @@ class M_attendance extends CI_Model
   public function where($cookie)
   {
     $where = "WHERE a.is_deleted = 0 ";
-    // if (@$cookie['search']['term'] != '') {
-    //   $where .= "AND a.attendance_name LIKE '%" . $this->db->escape_like_str($cookie['search']['term']) . "%' ";
-    // }
+    if (@$cookie['search']['term'] != '') {
+      $where .= "AND b.employee_name LIKE '%" . $this->db->escape_like_str($cookie['search']['term']) . "%' ";
+    }
     return $where;
   }
 
@@ -37,7 +37,16 @@ class M_attendance extends CI_Model
   {
     $where = "WHERE a.is_deleted = 0 ";
 
-    $sql = "SELECT * FROM attendance a $where ORDER BY created_at";
+    $sql = "SELECT 
+              a.*, b.employee_name, c.department_name, d.division_name, e.position_name, 
+              f.shift_id, f.shift_name
+            FROM attendance a 
+            JOIN employee b ON a.employee_id = b.employee_id
+            LEFT JOIN department c ON b.department_id = c.department_id
+            LEFT JOIN division d ON b.division_id = d.division_id
+            LEFT JOIN position e ON b.position_id = e.position_id
+            JOIN shift f ON a.shift_id = f.shift_id 
+            ORDER BY created_at";
     $query = $this->db->query($sql);
     return $query->result_array();
   }
@@ -46,7 +55,15 @@ class M_attendance extends CI_Model
   {
     $where = $this->where($cookie);
 
-    $sql = "SELECT COUNT(1) as total FROM attendance a $where";
+    $sql = "SELECT 
+              COUNT(1) as total
+            FROM attendance a 
+            JOIN employee b ON a.employee_id = b.employee_id
+            LEFT JOIN department c ON b.department_id = c.department_id
+            LEFT JOIN division d ON b.division_id = d.division_id
+            LEFT JOIN position e ON b.position_id = e.position_id
+            JOIN shift f ON a.shift_id = f.shift_id
+            $where";
     $query = $this->db->query($sql);
     return $query->row_array()['total'];
   }
