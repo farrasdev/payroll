f67fa34a43082e9d82baa3acd7fe2aa842401523
chrisnabayuaji <chrisnabayuaji@gmail.com>
2020-10-31 17:24:26 +0700

    Initial commit

--- application/modules/coeficient_all/controllers/Coeficient_all.php
+++ application/modules/coeficient_all/controllers/Coeficient_all.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Employee_expense extends MY_Controller
+class Coeficient_all extends MY_Controller
 {
 
   var $menu_id, $menu, $cookie;
@@ -12,18 +12,18 @@ class Employee_expense extends MY_Controller
 
     $this->load->model(array(
       'config/m_config',
-      'm_employee_expense',
+      'm_coeficient_all',
       'employee/m_employee'
     ));
 
-    $this->menu_id = '31.01';
+    $this->menu_id = '31.03';
     $this->menu = $this->m_config->get_menu($this->menu_id);
     if ($this->menu == null) redirect(site_url() . '/error/error_403');
 
     //cookie 
     $this->cookie = get_cookie_menu($this->menu_id);
     if ($this->cookie['search'] == null) $this->cookie['search'] = array('term' => '');
-    if ($this->cookie['order'] == null) $this->cookie['order'] = array('field' => 'employee_expense_date', 'type' => 'desc');
+    if ($this->cookie['order'] == null) $this->cookie['order'] = array('field' => 'coeficient_all_date', 'type' => 'desc');
     if ($this->cookie['per_page'] == null) $this->cookie['per_page'] = 10;
     if ($this->cookie['cur_page'] == null) 0;
   }
@@ -33,12 +33,12 @@ class Employee_expense extends MY_Controller
     authorize($this->menu, '_read');
     //cookie
     $this->cookie['cur_page'] = $this->uri->segment(3, 0);
-    $this->cookie['total_rows'] = $this->m_employee_expense->all_rows($this->cookie);
+    $this->cookie['total_rows'] = $this->m_coeficient_all->all_rows($this->cookie);
     set_cookie_menu($this->menu_id, $this->cookie);
     //main data
     $data['menu'] = $this->menu;
     $data['cookie'] = $this->cookie;
-    $data['main'] = $this->m_employee_expense->list_data($this->cookie);
+    $data['main'] = $this->m_coeficient_all->list_data($this->cookie);
     $data['pagination_info'] = pagination_info(count($data['main']), $this->cookie);
     //set pagination
     set_pagination($this->menu, $this->cookie);
@@ -54,7 +54,7 @@ class Employee_expense extends MY_Controller
       $data['main'] = null;
     } else {
       create_log(3, $this->menu['menu_name']);
-      $data['main'] = $this->m_employee_expense->by_field('employee_expense_id', $id);
+      $data['main'] = $this->m_coeficient_all->by_field('coeficient_all_id', $id);
     }
     $data['id'] = $id;
     $data['menu'] = $this->menu;
@@ -70,25 +70,25 @@ class Employee_expense extends MY_Controller
       $data['is_active'] = 0;
     }
     $data['nominal'] = num_sys($data['nominal']);
-    $data['employee_expense_date'] = reverse_date($data['employee_expense_date']);
-    $cek = $this->m_employee_expense->by_field('employee_expense_id', $data['employee_expense_id']);
+    $data['coeficient_all_date'] = reverse_date($data['coeficient_all_date']);
+    $cek = $this->m_coeficient_all->by_field('coeficient_all_id', $data['coeficient_all_id']);
     if ($id == null) {
       if ($cek != null) {
         $this->session->set_flashdata('flash_error', 'Kode sudah ada di sistem.');
         redirect(site_url() . '/' .  $this->menu['controller']  . '/form/');
       }
-      $data['employee_expense_id'] = $this->uuid->v4();
+      $data['coeficient_all_id'] = $this->uuid->v4();
       unset($data['old']);
-      $this->m_employee_expense->save($data, $id);
+      $this->m_coeficient_all->save($data, $id);
       create_log(2, $this->menu['menu_name']);
       $this->session->set_flashdata('flash_success', 'Kode berhasil ditambahkan.');
     } else {
-      if ($data['old'] != $data['employee_expense_id'] && $cek != null) {
+      if ($data['old'] != $data['coeficient_all_id'] && $cek != null) {
         $this->session->set_flashdata('flash_error', 'Kode sudah ada di sistem.');
         redirect(site_url() . '/' . $this->menu['controller'] . '/form/' . $id);
       }
       unset($data['old']);
-      $this->m_employee_expense->save($data, $id);
+      $this->m_coeficient_all->save($data, $id);
       create_log(3, $this->menu['menu_name']);
       $this->session->set_flashdata('flash_success', 'Data berhasil diubah.');
     }
@@ -98,7 +98,7 @@ class Employee_expense extends MY_Controller
   public function delete($id = null)
   {
     ($id == null) ? authorize($this->menu, '_create') : authorize($this->menu, '_update');
-    $this->m_employee_expense->delete($id);
+    $this->m_coeficient_all->delete($id);
     create_log(4, $this->menu['menu_name']);
     $this->session->set_flashdata('flash_success', 'Data berhasil dihapus.');
     redirect(site_url() . '/' . $this->menu['controller'] . '/' . $this->menu['url'] . '/' . $this->cookie['cur_page']);
@@ -108,9 +108,9 @@ class Employee_expense extends MY_Controller
   {
     authorize($this->menu, '_update');
     if ($type == 'enable') {
-      $this->m_employee_expense->update($id, array('is_active' => 1));
+      $this->m_coeficient_all->update($id, array('is_active' => 1));
     } else {
-      $this->m_employee_expense->update($id, array('is_active' => 0));
+      $this->m_coeficient_all->update($id, array('is_active' => 0));
     }
     create_log(3, $this->this->menu['menu_name']);
     redirect(site_url() . '/' . $this->menu['controller'] . '/' . $this->menu['url'] . '/' . $this->cookie['cur_page']);
@@ -124,21 +124,21 @@ class Employee_expense extends MY_Controller
         switch ($type) {
           case 'delete':
             authorize($this->menu, '_delete');
-            $this->m_employee_expense->delete($key);
+            $this->m_coeficient_all->delete($key);
             $flash = 'Data berhasil dihapus.';
             $t = 4;
             break;
 
           case 'enable':
             authorize($this->menu, '_update');
-            $this->m_employee_expense->update($key, array('is_active' => 1));
+            $this->m_coeficient_all->update($key, array('is_active' => 1));
             $flash = 'Data berhasil diaktifkan.';
             $t = 3;
             break;
 
           case 'disable':
             authorize($this->menu, '_update');
-            $this->m_employee_expense->update($key, array('is_active' => 0));
+            $this->m_coeficient_all->update($key, array('is_active' => 0));
             $flash = 'Data berhasil dinonaktifkan.';
             $t = 3;
             break;
@@ -154,11 +154,11 @@ class Employee_expense extends MY_Controller
   {
     if ($type == 'check_id') {
       $data = $this->input->post();
-      $cek = $this->m_employee_expense->by_field('employee_expense_id', $data['employee_expense_id']);
+      $cek = $this->m_coeficient_all->by_field('coeficient_all_id', $data['coeficient_all_id']);
       if ($id == null) {
         echo ($cek != null) ? 'false' : 'true';
       } else {
-        echo ($id != $data['employee_expense_id'] && $cek != null) ? 'false' : 'true';
+        echo ($id != $data['coeficient_all_id'] && $cek != null) ? 'false' : 'true';
       }
     }
   }
---
+++ application/modules/coeficient_all/views/form.php
@@ -0,0 +1,129 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-0">
+        <div class="col-sm-6">
+          <h5 class="m-0 text-dark"><i class="<?= @$menu['icon'] ?>"></i> <?= @$menu['menu_name'] ?></h5>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item active">Pengupahan</li>
+            <li class="breadcrumb-item active"><?= @$menu['menu_name'] ?></li>
+            <li class="breadcrumb-item active"><?= ($id == null) ? 'Tambah' : 'Ubah'; ?></li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div><!-- /.content-header -->
+  <!-- Main content -->
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Form <?= $menu['menu_name'] ?></h3>
+            </div>
+            <form id="form" action="<?= site_url() . '/' . $menu['controller'] . '/save/' . $id ?>" method="post" autocomplete="off" enctype="multipart/form-data">
+              <div class="card-body">
+                <div class="flash-error" data-flasherror="<?= $this->session->flashdata('flash_error') ?>"></div>
+                <input type="hidden" class="form-control form-control-sm" name="coeficient_all_id" id="coeficient_all_id" value="<?= @$main['coeficient_all_id'] ?>" required>
+                <?php if ($id != null) : ?>
+                  <input type="hidden" class="form-control form-control-sm" name="old" id="old" value="<?= @$main['coeficient_all_id'] ?>" required>
+                <?php endif; ?>
+                <div class="form-group row">
+                  <label class="col-sm-2 col-form-label text-right">Nama Pegawai <span class="text-danger">*</span></label>
+                  <div class="col-sm-4">
+                    <select class="form-control form-control-sm select2" name="employee_id" id="employee_id" required>
+                      <option value="">-- Pilih --</option>
+                      <?php foreach ($employee as $r) : ?>
+                        <option value="<?= $r['employee_id'] ?>" <?= (@$main['employee_id'] == $r['employee_id']) ? 'selected' : '' ?>><?= $r['employee_id'] . ' - ' . $r['employee_name'] ?></option>
+                      <?php endforeach; ?>
+                    </select>
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="icon" class="col-sm-2 col-form-label text-right">Tanggal <span class="text-danger">*</span></label>
+                  <div class="col-sm-2">
+                    <input type="text" class="form-control form-control-sm datepicker" name="coeficient_all_date" id="coeficient_all_date" value="<?= @reverse_date($main['coeficient_all_date']) ?>" required>
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="icon" class="col-sm-2 col-form-label text-right">Nominal <span class="text-danger">*</span></label>
+                  <div class="col-sm-2">
+                    <input type="text" class="form-control form-control-sm num-int" name="nominal" id="nominal" value="<?= @($main['nominal']) ?>" required>
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="icon" class="col-sm-2 col-form-label text-right">Keterangan</label>
+                  <div class="col-sm-6">
+                    <input type="text" class="form-control form-control-sm" name="description" id="description" value="<?= @$main['description'] ?>">
+                  </div>
+                </div>
+              </div>
+              <div class="card-footer">
+                <div class="row">
+                  <div class="col-md-10 offset-md-2">
+                    <button type="submit" class="btn btn-sm btn-primary btn-submit"><i class="fas fa-save"></i> Simpan</button>
+                    <a class="btn btn-sm btn-default btn-cancel" href="<?= site_url() . '/' . $menu['controller'] . '/' . $menu['url'] ?>"><i class="fas fa-times"></i> Batal</a>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </div><!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+<script>
+  $(document).ready(function() {
+    $("#form").validate({
+      rules: {
+        coeficient_all_id: {
+          remote: {
+            type: 'post',
+            url: "<?= site_url() . '/' . $menu['controller'] . '/ajax/check_id/' . $id ?>",
+            data: {
+              'coeficient_all_id': function() {
+                return $('#coeficient_all_id').val();
+              }
+            },
+            dataType: 'json'
+          }
+        }
+      },
+      messages: {
+        coeficient_all_id: {
+          remote: "Kode sudah digunakan"
+        }
+      },
+      errorElement: "em",
+      errorPlacement: function(error, element) {
+        error.addClass("invalid-feedback");
+        if (element.prop("type") === "checkbox") {
+          error.insertAfter(element.next("label"));
+        } else if ($(element).hasClass('select2')) {
+          error.insertAfter(element.next(".select2-container")).addClass('mt-1');
+        } else {
+          error.insertAfter(element);
+        }
+      },
+      highlight: function(element, errorClass, validClass) {
+        $(element).addClass("is-invalid").removeClass("is-valid");
+      },
+      unhighlight: function(element, errorClass, validClass) {
+        $(element).addClass("is-valid").removeClass("is-invalid");
+      },
+      submitHandler: function(form) {
+        $(".btn-submit").html('<i class="fas fa-spin fa-spinner"></i> Proses');
+        $(".btn-submit").addClass('disabled');
+        $(".btn-cancel").addClass('disabled');
+        form.submit();
+      }
+    });
+  })
+</script>
