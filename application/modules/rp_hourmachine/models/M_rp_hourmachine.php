63f36a5be7c5a6d7af52bc654a2fd80a87574ac1
chrisnabayuaji <chrisnabayuaji@gmail.com>
2020-10-31 08:05:48 +0700

    Initial commit

---
+++ application/modules/rp_hourmachine/models/M_rp_hourmachine.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_rp_hourmachine extends CI_Model
+{
+
+  public function get_data($start_date, $end_date)
+  {
+    $diff = date_difference($end_date, $start_date);
+    $data = $this->db->query(
+      "SELECT 
+        a.employee_id, a.employee_name 
+      FROM employee a 
+      WHERE is_active = 1 ORDER BY a.employee_id"
+    )->result_array();
+    foreach ($data as $k => $v) {
+      $attendance = array();
+      for ($i = 0; $i <= $diff; $i++) {
+        $cur_date = date('Y-m-d', strtotime($start_date . " + $i days"));
+        $attendance[$cur_date] = "0";
+        $att = $this->db->query("SELECT hourmachine FROM attendance WHERE employee_id = '" . $v['employee_id'] . "' AND attendance_date = '" . $cur_date . "'")->row_array();
+        if ($att != null) {
+          $attendance[$cur_date] = $att['hourmachine'];
+        }
+      }
+      $data[$k]['attendance'] = $attendance;
+    }
+    return $data;
+  }
+}
